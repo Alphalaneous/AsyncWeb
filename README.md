d678f03e4a92b3e445e70c88bcf0910e82042139
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-07-16 11:17:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# AsyncWeb
+
+Mod that makes web requests asynchronous.
+
+Allows you to do stuff like view comments while downloading a song.
+
+Currently this mod does not change functionality such as queued song/sfx downloads, though that is a potential point of interest for changing in the future.
+
+## Known Issues
+
+Progress bars for song downloads do not work, don't fret as it is still downloading.
---
+++ src/main.cpp
@@ -0,0 +1,101 @@
+#include <Geode/Geode.hpp>
+#include <Geode/utils/web.hpp>
+#include <Geode/modify/CCHttpClient.hpp>
+
+using namespace geode::prelude;
+
+std::map<CCHttpRequest*, EventListener<web::WebTask>*> m_downloadListeners;
+
+class $modify(MyCCHttpClient, CCHttpClient){
+
+	//here be dragons
+
+	/*
+		tldr; hooking the dtor didn't work, can't add fields to a CCObject
+		somewhere in the chain, the reference count of the CCHttpRequest is fucked
+		so I delete it, as I know it cannot be used anywhere else
+		There has to be a global map that I can reference as well to prevent the EventListener from going out of scope
+		and thus killing itself, normally I'd just add it as a field, but alas
+	*/
+
+	void send(CCHttpRequest* request){
+    	auto req = web::WebRequest();
+
+		std::vector<uint8_t> bytes;
+		for(int i = 0; i < request->getRequestDataSize(); i++){
+			bytes.push_back(static_cast<uint8_t>(request->getRequestData()[i]));
+		}
+	
+		req.userAgent("");
+		if(bytes.size() > 0){
+			req.body(bytes);
+		}
+		req.version(web::HttpVersion::VERSION_2_0);
+
+		EventListener<web::WebTask>* eventListener = new EventListener<web::WebTask>();
+
+		request->retain();
+		eventListener->bind([this, request](web::WebTask::Event* e){
+        	if (auto res = e->getValue()){
+				CCHttpResponse* oldResponse = new extension::CCHttpResponse(request);
+        		oldResponse->setSucceed(res->ok());
+				oldResponse->retain();
+
+				if (res->ok()) {
+					geode::Loader::get()->queueInMainThread([this, res, oldResponse, request](){
+						auto data = res->data();
+
+						std::vector<char>* charData = new std::vector<char>();
+						for (int i = 0; i < data.size(); i++) {
+							charData->push_back(data.at(i));
+						}
+						oldResponse->setResponseData(charData);
+						delete charData;
+						oldResponse->setResponseCode(res->code());
+
+						SEL_HttpResponse pSelector = request->getSelector();
+						CCObject* pTarget = request->getTarget();
+
+						if (pTarget && pSelector) {
+							(pTarget->*pSelector)(this, oldResponse);
+						}
+
+						oldResponse->release();
+						request->release();
+						
+						m_downloadListeners.erase(m_downloadListeners.find(request));
+						delete request;
+					});
+				}
+				else{
+					oldResponse->release();
+					request->release();
+					
+					m_downloadListeners.erase(m_downloadListeners.find(request));
+					delete request;
+				}
+				
+			}
+		});
+
+		web::WebTask webtask;
+
+		switch (request->getRequestType()) {
+			case CCHttpRequest::kHttpGet:
+				webtask = req.get(request->getUrl());
+				break;
+			case CCHttpRequest::kHttpPost:
+				webtask = req.post(request->getUrl());
+				break;
+			case CCHttpRequest::kHttpPut:
+				webtask = req.put(request->getUrl());
+				break;
+			default:
+				webtask = req.post(request->getUrl());
+
+		}
+		eventListener->setFilter(webtask);
+
+		m_downloadListeners[request] = eventListener;
+	}
+};
