5db327a5f357e6a96453f2277e7c22b280d524b5
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-07-16 20:32:38 -0400

    fix tabs/spaces

--- src/main.cpp
+++ src/main.cpp
@@ -8,76 +8,76 @@ std::map<CCHttpRequest*, EventListener<web::WebTask>*> m_downloadListeners;
 
 class $modify(MyCCHttpClient, CCHttpClient){
 
-	//here be dragons
+    //here be dragons
 
-	void send(CCHttpRequest* request){
-    	auto req = web::WebRequest();
+    void send(CCHttpRequest* request){
+        auto req = web::WebRequest();
 
-		gd::vector<uint8_t> bytes;
-		for(int i = 0; i < request->getRequestDataSize(); i++){
-			bytes.push_back(static_cast<uint8_t>(request->getRequestData()[i]));
-		}
-	
-		if(bytes.size() > 0){
-			req.body(bytes);
-		}
+        gd::vector<uint8_t> bytes;
+        for(int i = 0; i < request->getRequestDataSize(); i++){
+            bytes.push_back(static_cast<uint8_t>(request->getRequestData()[i]));
+        }
+    
+        if(bytes.size() > 0){
+            req.body(bytes);
+        }
 
-		req.userAgent("");
-		req.version(web::HttpVersion::VERSION_2_0);
+        req.userAgent("");
+        req.version(web::HttpVersion::VERSION_2_0);
 
-		EventListener<web::WebTask>* eventListener = new EventListener<web::WebTask>();
-		m_downloadListeners[request] = eventListener;
+        EventListener<web::WebTask>* eventListener = new EventListener<web::WebTask>();
+        m_downloadListeners[request] = eventListener;
 
-		CCHttpResponse* response = new CCHttpResponse(request);
-		response->autorelease();
+        CCHttpResponse* response = new CCHttpResponse(request);
+        response->autorelease();
 
-		eventListener->bind([this, request, eventListener, response](web::WebTask::Event* e){
-        	if (auto res = e->getValue()){
-				Loader::get()->queueInMainThread([this, res, request, eventListener, response](){
-					response->setSucceed(res->ok());
+        eventListener->bind([this, request, eventListener, response](web::WebTask::Event* e){
+            if (auto res = e->getValue()){
+                Loader::get()->queueInMainThread([this, res, request, eventListener, response](){
+                    response->setSucceed(res->ok());
 
-					if (res->ok()) {
-						auto data = res->data();
+                    if (res->ok()) {
+                        auto data = res->data();
 
-						gd::vector<char>* charData = new gd::vector<char>();
-						for (int i = 0; i < data.size(); i++) {
-							charData->push_back(data.at(i));
-						}
-						response->setResponseData(charData);
-						response->setResponseCode(res->code());
+                        gd::vector<char>* charData = new gd::vector<char>();
+                        for (int i = 0; i < data.size(); i++) {
+                            charData->push_back(data.at(i));
+                        }
+                        response->setResponseData(charData);
+                        response->setResponseCode(res->code());
 
-						SEL_HttpResponse pSelector = request->getSelector();
-						CCObject* pTarget = request->getTarget();
+                        SEL_HttpResponse pSelector = request->getSelector();
+                        CCObject* pTarget = request->getTarget();
 
-						if (pTarget && pSelector) {
-							(pTarget->*pSelector)(this, response);
-						}
+                        if (pTarget && pSelector) {
+                            (pTarget->*pSelector)(this, response);
+                        }
 
-						delete charData;
-					}
-					
-					m_downloadListeners.erase(m_downloadListeners.find(request));
-					delete eventListener;
-				});
-			}
-		});
+                        delete charData;
+                    }
+                    
+                    m_downloadListeners.erase(m_downloadListeners.find(request));
+                    delete eventListener;
+                });
+            }
+        });
 
-		web::WebTask webtask;
+        web::WebTask webtask;
 
-		switch (request->getRequestType()) {
-			case CCHttpRequest::kHttpGet:
-				webtask = req.get(request->getUrl());
-				break;
-			case CCHttpRequest::kHttpPost:
-				webtask = req.post(request->getUrl());
-				break;
-			case CCHttpRequest::kHttpPut:
-				webtask = req.put(request->getUrl());
-				break;
-			default:
-				webtask = req.post(request->getUrl());
-		}
+        switch (request->getRequestType()) {
+            case CCHttpRequest::kHttpGet:
+                webtask = req.get(request->getUrl());
+                break;
+            case CCHttpRequest::kHttpPost:
+                webtask = req.post(request->getUrl());
+                break;
+            case CCHttpRequest::kHttpPut:
+                webtask = req.put(request->getUrl());
+                break;
+            default:
+                webtask = req.post(request->getUrl());
+        }
 
-		eventListener->setFilter(webtask);
-	}
+        eventListener->setFilter(webtask);
+    }
 };
