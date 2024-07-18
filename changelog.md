65a74c4e66e25286bd01c6e3271151cd3476e02d
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-07-17 20:46:39 -0400

    Android works!

--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
+# 0.1.0-alpha.4
+- Add android support back (Thank you @sleepyut).
+
 # 0.1.0-alpha.3
 - Remove android support.
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,31 +6,7 @@ using namespace geode::prelude;
 
 /*
 
-    After hours of trying, something for some reason makes this mod not work on android. 
-    I have tried everything, to no avail. This mod was not destined for an android release
-    for android is the strongest form of evil known to man. There will likely never be android
-    support unless an angel pops in and resolves all the horrors within this small but powerful
-    mod here. I hope that anyone that tries to resolve the issues here can do so peacefully,
-    unlike me, who is ready to explode. 
-
-    I sit here in my chair, staring at the code, wondering what even brought me here. It started
-    back in the earlier days of Geometry Dash modding. Back in 2.1, I made a mod that ported Axmol's
-    HttpClient to the game, which allowed asynchronous requests to work. This mod was called
-    ConcurrentHTTP. I thought porting this to 2.2 would be easy, and that there wouldn't be trouble,
-    but oh man was I wrong. 
-    
-    This mod has led to me realizing that quite a bit of people in the Geode discord server can be a 
-    bit dickish. People tend to say my code is bad without ever telling me what it is exactly or why
-    it is bad. It's always in indirect comments that are almost exactly describing me. When I bring 
-    up that I want to know what I am doing wrong and why my code is "Shit", I'm hit with silence or
-    being told they don't want to tell me. I do not know how to improve if the people around me
-    refuse to tell me what I am doing wrong. 
-
-    If you are reading this right now, I don't know how to code, everything you see is just be knowing
-    logic and piecing things together. I do not retain actual information, just syntax and order. 
-    Most of my projects are hours of Googling solutions that I feel will work together for a full
-    project, but alas I am met with the realization that nothing I make is good, nor worthwhile in
-    the end, and that it is dogshit compared to real programmers. 
+    @sleepyut is based, thanks for helping fix this mess to work for android!!
 
 */
 
@@ -43,7 +19,7 @@ class $modify(MyCCHttpClient, CCHttpClient) {
     void send(CCHttpRequest* request) {
         auto req = web::WebRequest();
 
-        auto start = reinterpret_cast<uint8_t *>(request->getRequestData());
+        auto start = reinterpret_cast<uint8_t*>(request->getRequestData());
         std::vector bytes(start, start + request->getRequestDataSize());
     
         if (!bytes.empty()){
